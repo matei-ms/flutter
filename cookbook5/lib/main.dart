3874a8473e43044303bcefc13080b4caa41b55cf
M. Matei <8888msm8888@gmail.com>
2018-07-08 13:55:17 +0300

    merge

---
+++ cookbook5/lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Custom Fonts',
+      // Set Raleway as the default app font
+      theme: ThemeData(fontFamily: 'Raleway'),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // The AppBar will use the app-default Raleway font
+      appBar: AppBar(title: Text('Custom Fonts')),
+      body: Center(
+        // This Text Widget will use the RobotoMono font
+        child: Text(
+          'Roboto Mono sample',
+          style: TextStyle(fontFamily: 'RobotoMono'),
+        ),
+      ),
+    );
+  }
+}
